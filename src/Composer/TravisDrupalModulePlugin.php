42ac2562933597ab1fe4078db00f4b902bc40569
Mateu Aguiló Bosch <mateu@mateuaguilo.com>
2019-10-11 07:41:04 +0200

    fix: add static code analysis tools

--- src/Composer/TravisDrupalModulePlugin.php
+++ src/Composer/TravisDrupalModulePlugin.php
@@ -1,10 +1,20 @@
 <?php
 
+/**
+ * Implements a composer plugin.
+ * php version 7.1
+ *
+ * @file     Composer plugin implementation file.
+ * @category Composer
+ * @package  TravisDrupalModule
+ * @author   Mateu Aguiló Bosch (e0ipso) <mateu@mateuaguilo.com>
+ * @license  https://choosealicense.com/licenses/gpl-2.0/ GNU/GPL-v2
+ * @link     https://mateuaguilo.com
+ */
+
 namespace TravisDrupalModule\Composer;
 
 use Composer\Composer;
-use Composer\DependencyResolver\Operation\InstallOperation;
-use Composer\DependencyResolver\Operation\UpdateOperation;
 use Composer\EventDispatcher\EventSubscriberInterface;
 use Composer\Installer\PackageEvent;
 use Composer\Installer\PackageEvents;
@@ -12,204 +22,310 @@ use Composer\IO\IOInterface;
 use Composer\Package\PackageInterface;
 use Composer\Package\RootPackageInterface;
 use Composer\Plugin\PluginInterface;
-use Composer\Script\Event;
 use Composer\Script\ScriptEvents;
-use RecursiveDirectoryIterator;
+use \LogicException;
+use \RecursiveDirectoryIterator;
 use Symfony\Component\Filesystem\Filesystem;
 use Webmozart\PathUtil\Path;
 
-final class TravisDrupalModulePlugin implements PluginInterface, EventSubscriberInterface {
+/**
+ * Composer plugin that copies the templates to their right location.
+ *
+ * @category Composer
+ * @package  TravisDrupalModule
+ * @author   Mateu Aguiló Bosch (e0ipso) <mateu@mateuaguilo.com>
+ * @license  https://choosealicense.com/licenses/gpl-2.0/ GNU/GPL-v2
+ * @link     https://mateuaguilo.com
+ *
+ * @SuppressWarnings(PHPMD.StaticAccess)
+ */
+final class TravisDrupalModulePlugin implements
+  PluginInterface,
+  EventSubscriberInterface
+{
 
-  const PACKAGE_NAME = 'e0ipso/travis-drupal-module';
+    const PACKAGE_NAME = 'e0ipso/travis-drupal-module';
 
-  const GLUE = "\n  * ";
+    const GLUE = "\n  * ";
 
-  /**
-   * @var Composer
-   */
-  private $composer;
+    /**
+     * The composer API.
+     *
+     * @var Composer
+     */
+    private $_composer;
 
-  /**
-   * @var IOInterface
-   */
-  private $io;
+    /**
+     * IO interface to log messages to the terminal.
+     *
+     * @var IOInterface
+     */
+    private $_io;
 
-  /**
-   * @var bool
-   */
-  private $scheduled = FALSE;
+    /**
+     * Keeps state of weather or not the operations are scheduled.
+     *
+     * @var bool
+     */
+    private $_scheduled = false;
 
-  /**
-   * @var string[]
-   */
-  private $pathMappings;
+    /**
+     * Key value store for path mappings between origin and destination paths.
+     *
+     * @var string[]
+     */
+    private $_pathMappings;
 
-  /**
-   * {@inheritdoc}
-   */
-  public function activate(Composer $composer, IOInterface $io) {
-    $this->composer = $composer;
-    $this->io = $io;
-  }
-
-  /**
-   * Attach package installation events:.
-   *
-   * {@inheritdoc}
-   */
-  public static function getSubscribedEvents(): array {
-    return [
-      PackageEvents::POST_PACKAGE_INSTALL => 'postPackageInstall',
-      PackageEvents::POST_PACKAGE_UPDATE => 'postPackageUpdate',
-      ScriptEvents::POST_INSTALL_CMD => 'postInstallCmd',
-      ScriptEvents::POST_UPDATE_CMD => 'postUpdateCmd',
-    ];
-  }
-
-  /**
-   * When this package is updated, the git hook is also initialized.
-   */
-  public function postPackageInstall(PackageEvent $event) {
-    $operation = $event->getOperation();
-    assert($operation instanceof InstallOperation);
-    $package = $operation->getPackage();
-
-    if (!$this->isCurrentPackage($package)) {
-      return;
+    /**
+     * Apply plugin modifications to Composer
+     *
+     * @param Composer    $composer The composer API.
+     * @param IOInterface $theIo    IO interface to log to the terminal.
+     *
+     * @return void
+     */
+    public function activate(Composer $composer, IOInterface $theIo)
+    {
+        $this->_composer = $composer;
+        $this->_io = $theIo;
     }
 
-    // Schedule init when command is completed
-    $this->scheduled = TRUE;
-  }
-
-  /**
-   * When this package is updated, the git hook is also updated.
-   */
-  public function postPackageUpdate(PackageEvent $event) {
-    $operation = $event->getOperation();
-    assert($operation instanceof UpdateOperation);
-    $package = $operation->getTargetPackage();
-
-    if (!$this->isCurrentPackage($package)) {
-      return;
+    /**
+     * Attach package installation events:.
+     *
+     * @return string[]
+     */
+    public static function getSubscribedEvents(): array
+    {
+        return [
+        PackageEvents::POST_PACKAGE_INSTALL => 'postChangeInstall',
+        PackageEvents::POST_PACKAGE_UPDATE => 'postChangeInstall',
+        ScriptEvents::POST_INSTALL_CMD => 'postInstallCmd',
+        ScriptEvents::POST_UPDATE_CMD => 'postUpdateCmd',
+        ];
     }
 
-    // Schedule init when command is completed
-    $this->scheduled = TRUE;
-  }
+    /**
+     * When this package is changed, the files are also initialized.
+     *
+     * @param \Composer\Installer\PackageEvent $event The composer event.
+     *
+     * @return void
+     */
+    public function postChangeInstall(PackageEvent $event)
+    {
+        $operation = $event->getOperation();
+        $package = $operation->getPackage();
 
-  public function postInstallCmd(Event $event) {
-    if (!$this->scheduled) {
-      return;
+        if (!$this->_isCurrentPackage($package)) {
+            return;
+        }
+
+        // Schedule init when command is completed
+        $this->_scheduled = true;
     }
-    $this->createRequiredFiles($event);
-  }
 
-  public function postUpdateCmd(Event $event) {
-    if (!$this->scheduled) {
-      return;
+    /**
+     * After running an install command on this app we may need to initialize.
+     *
+     * @return void
+     */
+    public function postInstallCmd()
+    {
+        if (!$this->_scheduled) {
+            return;
+        }
+        $this->_createRequiredFiles();
     }
-    try {
-      $this->createRequiredFiles($event);
+
+    /**
+     * After running an update command on this app we may need to initialize.
+     *
+     * @return void
+     */
+    public function postUpdateCmd()
+    {
+        if (!$this->_scheduled) {
+            return;
+        }
+        try {
+            $this->_createRequiredFiles();
+        } catch (\LogicException $exception) {
+            $this->_io
+                ->writeError('<fg=red>' . $exception->getMessage() . '</fg=red>');
+        }
     }
-    catch (\LogicException $exception) {
-      $this->io->writeError('<fg=red>' . $exception->getMessage() . '</fg=red>');
+
+    /**
+     * Checks if the provided package is the current package.
+     *
+     * @param \Composer\Package\PackageInterface $package The package object.
+     *
+     * @return bool
+     *   TRUE if the package is the current one.
+     */
+    private function _isCurrentPackage(PackageInterface $package): bool
+    {
+        return self::PACKAGE_NAME === $package->getName();
     }
-  }
 
-  private function isCurrentPackage(PackageInterface $package): bool {
-    return self::PACKAGE_NAME === $package->getName();
-  }
+    /**
+     * Creates the necessary files from the templates directory.
+     *
+     * @return void
+     */
+    private function _createRequiredFiles()
+    {
+        $fsystem = new Filesystem();
 
-  private function createRequiredFiles(Event $event) {
-    $fs = new Filesystem();
+        $vendorDir = $this->_composer->getConfig()->get('vendor-dir');
+        $rootDir = Path::getDirectory($vendorDir);
 
-    $vendorDir = $this->composer->getConfig()->get('vendor-dir');
-    $rootDir = Path::getDirectory($vendorDir);
+        $currentPackage = $this->_composer
+            ->getRepositoryManager()
+            ->findPackage(static::PACKAGE_NAME, "*");
+        $packageDir = $this->_composer->getInstallationManager()
+            ->getInstallPath($currentPackage);
 
-    $currentPackage = $this->composer
-      ->getRepositoryManager()
-      ->findPackage(static::PACKAGE_NAME, "*");
-    $packageDir = $this->composer->getInstallationManager()
-      ->getInstallPath($currentPackage);
+        // The GrumPHP file is generate on install, we don't want that one. We want
+        // out own version.
+        $grumphpFilename = Path::join($rootDir, 'grumphp.yml');
+        if ($fsystem->exists($grumphpFilename)) {
+            $fsystem->remove($grumphpFilename);
+        }
+        $this->_validateTemplateLocations($fsystem, $packageDir, $rootDir);
+        $this->_copyTemplatesToRoot($fsystem, $packageDir, $rootDir);
 
-    // The GrumPHP file is generate on install, we don't want that one. We want
-    // out own version.
-    $grumphpFilename = Path::join($rootDir, 'grumphp.yml');
-    if ($fs->exists($grumphpFilename)) {
-      $fs->remove($grumphpFilename);
+        // Leave a message of what we have done.
+        $rootPackage = $this->_composer->getPackage();
+        assert($rootPackage instanceof RootPackageInterface);
+        $msg = static::GLUE . implode(
+            static::GLUE, array_values(
+                $this->_computePathMappings($packageDir, $rootDir)
+            )
+        );
+        $tpl  = "\n<fg=green>%s successfully copied the following files in ";
+        $tpl .= "%s:%s</fg=green>\n";
+        $this->_io->write(
+            sprintf(
+                $tpl,
+                static::PACKAGE_NAME,
+                $rootPackage->getName(),
+                $msg
+            )
+        );
     }
-    $this->validateTemplateLocations($fs, $packageDir, $rootDir);
-    $this->copyTemplatesToRoot($fs, $packageDir, $rootDir);
 
-    // Leave a message of what we have done.
-    $rootPackage = $this->composer->getPackage();
-    assert($rootPackage instanceof RootPackageInterface);
-    $msg = static::GLUE . implode(static::GLUE, array_values($this->computePathMappings($packageDir, $rootDir)));
-    $this->io->write(sprintf(
-      "\n<fg=green>%s successfully copied the following files in %s:%s</fg=green>\n",
-      static::PACKAGE_NAME,
-      $rootPackage->getName(),
-      $msg
-    ));
-  }
+    /**
+     * Given a source dir and a destination create the mappings of files to copy.
+     *
+     * @param string $packageDir The absolute path for this package.
+     * @param string $rootDir    The absolute path for the root directory.
+     *
+     * @return string[]
+     *   The keys are the origin paths and the values the destination paths.
+     */
+    private function _computePathMappings($packageDir, $rootDir)
+    {
+        if (isset($this->_pathMappings)) {
+            return $this->_pathMappings;
+        }
+        $templateDir = Path::join($packageDir, 'templates');
+        $flags = RecursiveDirectoryIterator::SKIP_DOTS
+          | RecursiveDirectoryIterator::CURRENT_AS_PATHNAME;
+        $iterator = new RecursiveDirectoryIterator(
+            $templateDir,
+            $flags
+        );
+        $this->_pathMappings = [];
+        foreach ($iterator as $entry) {
+            $destination = Path::join(
+                $rootDir,
+                Path::makeRelative($entry, $templateDir)
+            );
+            $this->_pathMappings[$entry] = $destination;
+        }
+        return $this->_pathMappings;
+    }
 
-  private function computePathMappings($packageDir, $rootDir) {
-    if (isset($this->pathMappings)) {
-      return $this->pathMappings;
+    /**
+     * Ensures that there are no conflicts when copying files.
+     *
+     * @param Filesystem $fsystem    The filesystem component.
+     * @param string     $packageDir The absolute path for the this package.
+     * @param string     $rootDir    The absolute path for the root directory.
+     *
+     * @return void
+     */
+    private function _validateTemplateLocations(
+        Filesystem $fsystem,
+        $packageDir,
+        $rootDir
+    ) {
+        $files = array_values($this->_computePathMappings($packageDir, $rootDir));
+        $existing_files = array_filter(
+            $files, function (string $file) use ($fsystem) {
+                return $fsystem->exists($file);
+            }
+        );
+        if (empty($existing_files)) {
+            return;
+        }
+        $message = 'Unable to proceed the following files already exist. Back ';
+        $message .= 'them up and delete them to re-generate them:';
+        $message .= static::GLUE . implode(static::GLUE, $existing_files);
+        throw new LogicException($message);
     }
-    $templateDir = Path::join($packageDir, 'templates');
-    $iterator = new RecursiveDirectoryIterator(
-      $templateDir,
-      RecursiveDirectoryIterator::SKIP_DOTS | RecursiveDirectoryIterator::CURRENT_AS_PATHNAME
-    );
-    $this->pathMappings = [];
-    foreach ($iterator as $entry) {
-      $destination = Path::join(
-        $rootDir,
-        Path::makeRelative($entry, $templateDir)
-      );
-      $this->pathMappings[$entry] = $destination;
-    }
-    return $this->pathMappings;
-  }
 
-  private function validateTemplateLocations(Filesystem $fs, $packageDir, $rootDir) {
-    $files = array_values($this->computePathMappings($packageDir, $rootDir));
-    $existing_files = array_filter($files, function (string $file) use ($fs) {
-      return $fs->exists($file);
-    });
-    if (empty($existing_files)) {
-      return;
+    /**
+     * Copies all files from the templates folder to the root directory.
+     *
+     * @param Filesystem $fsystem    The filesystem component.
+     * @param string     $packageDir The absolute path for the this package.
+     * @param string     $rootDir    The absolute path for the root directory.
+     *
+     * @return void
+     */
+    private function _copyTemplatesToRoot(Filesystem $fsystem, $packageDir, $rootDir)
+    {
+        $mappings = $this->_computePathMappings($packageDir, $rootDir);
+        foreach ($mappings as $source => $destination) {
+            $this->_copyRecursive($fsystem, $source, $destination);
+        }
     }
-    $message = 'Unable to proceed the following files already exist. Back them up and delete them to re-generate them:' . static::GLUE . implode(static::GLUE, $existing_files);
-    throw new \LogicException($message);
-  }
 
-  private function copyTemplatesToRoot(Filesystem $fs, $packageDir, $rootDir) {
-    foreach ($this->computePathMappings($packageDir, $rootDir) as $source => $destination) {
-      $this->copyRecursive($fs, $source, $destination);
+    /**
+     * Copies a source to a destination.
+     *
+     * If the source is a directory, then everything in there is copied as well.
+     *
+     * @param Filesystem $fsystem     The filesystem component.
+     * @param string     $source      The absolute path for the source.
+     * @param string     $destination The absolute path for the destination.
+     *
+     * @return void
+     */
+    private function _copyRecursive(Filesystem $fsystem, $source, $destination)
+    {
+        if (!is_dir($source)) {
+            $fsystem->copy($source, $destination);
+            return;
+        }
+        // Make the destination directory and copy all the things in there.
+        $flags = RecursiveDirectoryIterator::SKIP_DOTS
+        | RecursiveDirectoryIterator::CURRENT_AS_PATHNAME;
+        $iterator = new RecursiveDirectoryIterator(
+            $source,
+            $flags
+        );
+        $fsystem->mkdir($destination);
+        foreach ($iterator as $entry) {
+            $new_destination = Path::join(
+                $destination,
+                Path::makeRelative($entry, $source)
+            );
+            $this->_copyRecursive($fsystem, $entry, $new_destination);
+        }
     }
-  }
-
-  private function copyRecursive(Filesystem $fs, $source, $destination) {
-    if (!is_dir($source)) {
-      $fs->copy($source, $destination);
-      return;
-    }
-    // Make the destination directory and copy all the things in there.
-    $iterator = new RecursiveDirectoryIterator(
-      $source,
-      RecursiveDirectoryIterator::SKIP_DOTS | RecursiveDirectoryIterator::CURRENT_AS_PATHNAME
-    );
-    $fs->mkdir($destination);
-    foreach ($iterator as $entry) {
-      $new_destination = Path::join(
-        $destination,
-        Path::makeRelative($entry, $source)
-      );
-      $this->copyRecursive($fs, $entry, $new_destination);
-    }
-  }
 
 }
