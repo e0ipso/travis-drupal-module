6cce0844f682e745f890cea3ac29714705731ace
Mateu Aguiló Bosch <mateu@mateuaguilo.com>
2020-02-27 16:51:32 +0100

    fix: install the sut with a link

--- templates/.travis/scripts/execute-tests.sh
+++ templates/.travis/scripts/execute-tests.sh
@@ -4,7 +4,6 @@ DOCROOT=$1
 MODULE_NAME=$(basename ${TRAVIS_BUILD_DIR})
 PHPUNIT=$(dirname ${DOCROOT})/$(composer config bin-dir)/phpunit
 COMPOSER="$(which composer)"
-PACKAGE_NAME=$(cat ${TRAVIS_BUILD_DIR}/composer.json|jq '.name'|sed -e 's:"::g')
 
 # Define the color scheme.
 FG_C='\033[1;37m'
@@ -16,8 +15,8 @@ NO_C='\033[0m'
 # Link the current module using composer. Otherwise the autoload will not have the necessary classes
 # for the Unit tests.
 cd ${DOCROOT}/.. || exit 2
-echo -e "${FG_C}${BG_C} EXECUTING ${NO_C} ${COMPOSER} require ${PACKAGE_NAME} \"phpunit/phpunit:^6.5\" --no-interaction --no-progress --no-suggest\n\n"
-${COMPOSER} require ${PACKAGE_NAME} "phpunit/phpunit:^6.5" --no-interaction --no-progress --no-suggest
+echo -e "${FG_C}${BG_C} EXECUTING ${NO_C} ${COMPOSER} require \"phpunit/phpunit:^6.5\" --no-interaction --no-progress --no-suggest\n\n"
+${COMPOSER} require "phpunit/phpunit:^6.5" --no-interaction --no-progress --no-suggest
 
 # Execute the static code analysis tasks.
 cd ${TRAVIS_BUILD_DIR} || exit 2
@@ -26,6 +25,7 @@ php ./vendor/bin/grumphp run || exit 1
 
 # Execute all the tests.
 cd ${DOCROOT} || exit 2
+ln -s ${TRAVIS_BUILD_DIR} ${DOCROOT}/modules/contrib/${MODULE_NAME}
 perl -pe "s:\{DRUPAL_ROOT\}:${DOCROOT}:g" ${DOCROOT}/modules/contrib/${MODULE_NAME}/phpunit.xml.dist > ${DOCROOT}/modules/contrib/${MODULE_NAME}/phpunit.xml
 MINK_DRIVER_ARGS_WEBDRIVER='["chrome", {"chrome": {"switches":["headless"]}}, "http://localhost:4444"]' php ${PHPUNIT} \
   --configuration ${DOCROOT}/modules/contrib/${MODULE_NAME}/phpunit.xml \
